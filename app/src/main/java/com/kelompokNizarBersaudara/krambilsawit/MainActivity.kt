de03b01c452921c3e6d5500cb7cec892552e54b7
Nizar Fadlan <fadlannizar5@gmail.com>
2023-12-12 15:07:03 +0700

    Change main activity and ui sign in, sign up

--- app/src/main/java/com/kelompokNizarBersaudara/krambilsawit/MainActivity.kt
+++ app/src/main/java/com/kelompokNizarBersaudara/krambilsawit/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.kelompokNizarBersaudara.krambilsawit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.kelompokNizarBersaudara.krambilsawit.databinding.ActivityMainBinding
+import com.kelompokNizarBersaudara.krambilsawit.model.BlogPost
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var manager: LinearLayoutManager
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: BlogPostAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
